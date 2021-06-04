0676a8d3099e388716efa6adc838f7fb4101d659
Ry Ferguson <rytf12@gmail.com>
2021-06-04 04:06:18 -0700

    small clean-ups to front controller

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+
+//
 require('dotenv').config()
+
+//
 const express = require('express')
 const path = require('path')
-var cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const activeRouter = require('./routes/active')
 const apiRouter = require('./routes/api')
 const uuid = require('uuid/v4')
@@ -9,10 +13,12 @@ const session = require('express-session')
 const redisStore = require('connect-redis')(session)
 const redis = require('redis')
 
+//
 const redisClient = redis.createClient(process.env.REDIS_PORT)
 const app = express()
 const server = app.listen(process.env.HTTP_PORT)
 
+//
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -37,5 +43,3 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use('/', activeRouter)
 app.use('/api/v1/', apiRouter)
-
-//app.locals.pretty = true
