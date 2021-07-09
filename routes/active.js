d68aed8ee271e9d19a80feb69cf4d275da77cafa
Ry Ferguson <rytf12@gmail.com>
2021-07-09 02:48:31 -0700

    move processPostTags() out of routes file and into misc.js

--- routes/active.js
+++ routes/active.js
@@ -672,7 +672,7 @@ router.route('/new')
                 }
 
                 //
-                let [trimTags, tagErrors] = processPostTags(req.body.tags)
+                let [trimTags, tagErrors] = myMisc.processPostTags(req.body.tags)
                 errors = errors.concat(tagErrors)
 
                 //
@@ -790,7 +790,7 @@ router.route(/^\/p\/([a-z0-9]{22})\/edit$/i)
                 }
 
                 //
-                let [trimTags, tagErrors] = processPostTags(req.body.tags)
+                let [trimTags, tagErrors] = myMisc.processPostTags(req.body.tags)
                 errors = errors.concat(tagErrors)
 
                 //
@@ -912,50 +912,6 @@ router.route(/^\/c\/([a-z0-9]{22})\/edit$/i)
             }
         }
     )
-
-//
-function processPostTags(rTags) {
-    let tags = rTags.split(',')
-    let trimTags = []
-    let errors = []
-
-    for(let i = 0; i < tags.length; ++i) {
-        let trimTag = tags[i].trim().toLowerCase()
-
-        if(trimTag !== "" && trimTags.indexOf(trimTag) == -1) {
-            trimTags.push(trimTag)
-        }
-    }
-
-    //
-    let isCharError = false
-    let isLenError = false
-
-    for(let i = 0; i < trimTags.length; ++i) {
-        let isMatch = trimTags[i].match(/^[0-9a-zA-Z-]+$/)
-
-        if(!isCharError && isMatch === null) {
-            errors.push({'msg': 'group names can only contain numbers, letters and dashes'})
-            isCharError = true
-        }
-
-        let tagLen = trimTags[i].length
-        let isLenOkay = tagLen >= 3 && tagLen <= 20
-
-        if(!isLenError && !isLenOkay) {
-            errors.push({'msg': 'each group name must be 3-20 characters'})
-            isLenError = true
-        }
-    }
-
-    //
-    if(trimTags.length > 4) {
-        errors.push({'msg': 'the max tags per post is 4'})
-    }
-
-    //
-    return [trimTags, errors]
-}
 
 //
 async function createPostTags(trimTags, postId) {
