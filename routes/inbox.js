09747c17245e05569d736cf191a452f7a7d57fbd
Ry Ferguson <rytf12@gmail.com>
2021-07-20 09:04:06 -0700

    move inbox route into its own file

---
+++ routes/inbox.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const db = require('../db')
+const myMisc = require('../misc.js')
+
+const router = express.Router()
+
+router.route('/')
+    .get(async (req, res) => {
+        if(req.session.user) {
+
+            //
+            const filterUserId = await db.getCurrEyesId(req)
+            const isDiscoverMode = myMisc.isDiscover(req)
+
+            //
+            let page = 1
+
+            if(typeof req.query.p !== 'undefined') {
+                page = parseInt(req.query.p)
+
+                if(isNaN(page)) {
+                    return res.redirect('/inbox')
+                }
+            }
+
+            //
+            const{rows:comments} = await db.getInboxComments(
+                myMisc.getCurrTimeZone(req),
+                req.session.user.user_id,
+                isDiscoverMode,
+                filterUserId,
+                page)
+
+            //
+            res.render(
+                'inbox',
+                {
+                    html_title: 'Inbox',
+                    user: req.session.user,
+                    comments: comments,
+                    page: page,
+                    is_discover_mode: isDiscoverMode,
+                    comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
+                    max_width: myMisc.getCurrSiteMaxWidth(req)
+                }
+            )
+        }
+        else {
+            res.render(
+                'message',
+                {
+                    html_title: 'Inbox',
+                    message: "<a href=\"/login\">Log in</a> to view your inbox.",
+                    user: req.session.user,
+                    max_width: myMisc.getCurrSiteMaxWidth(req)
+                })
+        }
+    })
+
+module.exports = router
