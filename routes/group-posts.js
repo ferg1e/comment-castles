2220182b54591f88f2bb7795b326ad2c7e175d8c
Ry Ferguson <rytf12@gmail.com>
2021-07-20 07:58:23 -0700

    move the 'group posts' route into its own file

---
+++ routes/group-posts.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const db = require('../db')
+const myMisc = require('../misc.js')
+
+const router = express.Router({mergeParams: true})
+
+router.get(
+    '/',
+    async (req, res) => {
+        const tag = req.params[0]
+        let finalUserId = req.session.user ? req.session.user.user_id : -1
+
+        //
+        let page = 1
+
+        if(typeof req.query.p !== 'undefined') {
+            page = parseInt(req.query.p)
+
+            if(isNaN(page)) {
+                return res.redirect(`/${tag}`)
+            }
+        }
+
+        //
+        const isDiscoverMode = myMisc.isDiscover(req)
+        const filterUserId = await db.getCurrEyesId(req)
+
+        //
+        const {rows} = await db.getTagPosts(
+            finalUserId,
+            myMisc.getCurrTimeZone(req),
+            page,
+            tag,
+            isDiscoverMode,
+            filterUserId)
+
+        res.render(
+            'posts2',
+            {
+                html_title: tag,
+                user: req.session.user,
+                posts: rows,
+                page: page,
+                base_url: `/r/${tag}`,
+                is_discover_mode: isDiscoverMode,
+                max_width: myMisc.getCurrSiteMaxWidth(req),
+                page_tag: tag
+            })
+    }
+)
+
+module.exports = router
