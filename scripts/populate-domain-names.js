f32be7b941839f05abd662d74b6288aa82c8bbdb
Ry Ferguson <rytf12@gmail.com>
2021-07-25 06:45:13 -0700

    DRY up populate domain names script

--- scripts/populate-domain-names.js
+++ scripts/populate-domain-names.js
@@ -3,31 +3,17 @@
 require('dotenv').config()
 
 //
-const {URL} = require('url');
+const myMisc = require('../misc.js')
 const db = require('../db')
 
 db.getPostLinks().then(async res => {
     for(let i = 0; i < res.rows.length; ++i) {
         const link = res.rows[i]['link']
         const postId = res.rows[i]['post_id']
-        const myUrl = new URL(link)
+        const domainName = myMisc.getDomainName(link)
+        const domainNameId = await db.getDomainNameId(domainName)
 
-        // remove leading www.
-        const domainName = myUrl.hostname.replace(/^(www\.)/, '')
-
-        db.getDomainName(domainName).then(async res => {
-            const isExist = res.rows.length
-
-            if(isExist) {
-                console.log('existing domain name')
-                await db.updatePostDomainNameId(postId, res.rows[0].domain_name_id)
-            }
-            else {
-                console.log('new domain name')
-
-                const newDomain = await db.createDomainName(domainName)
-                await db.updatePostDomainNameId(postId, newDomain.rows[0]['domain_name_id'])
-            }
-        })
+        await db.updatePostDomainNameId(postId, domainNameId)
+        console.log([postId, link, domainName, domainNameId])
     }
 })
