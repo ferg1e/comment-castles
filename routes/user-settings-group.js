b836c3e9714f92692f1ca080a76bfd6a65f07957
Ry Ferguson <rytf12@gmail.com>
2021-09-08 08:17:13 -0700

    individual private group settings: display 'add user' form; set up boilerplate for POST route

--- routes/user-settings-group.js
+++ routes/user-settings-group.js
@@ -27,7 +27,8 @@ router.route('/')
                     {
                         html_title: htmlTitle,
                         user: req.session.user,
-                        max_width: myMisc.getCurrSiteMaxWidth(req)
+                        max_width: myMisc.getCurrSiteMaxWidth(req),
+                        errors: []
                     })
             }
             else {
@@ -36,6 +37,42 @@ router.route('/')
         }
         else {
             res.send('private group does not exist')
+        }
+    })
+    .post(async (req, res) => {
+
+        //
+        if(req.session.user) {
+
+            //
+            const {rows:data1} = await db.getPrivateGroupWithName(req.query.name)
+
+            //
+            if(data1.length) {
+
+                //
+                const privateGroup = data1[0]
+
+                if(privateGroup.created_by == req.session.user.user_id) {
+                    res.render(
+                        'my-settings-group',
+                        {
+                            html_title: htmlTitle,
+                            user: req.session.user,
+                            max_width: myMisc.getCurrSiteMaxWidth(req),
+                            errors: []
+                        })
+                }
+                else {
+                    res.send('hello...')
+                }
+            }
+            else {
+                res.send('private group does not exist')
+            }
+        }
+        else {
+            res.send('please log in')
         }
     })
 
