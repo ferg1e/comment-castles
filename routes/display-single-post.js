cdb6bbcb8ce5020209d99d41ff35d4c972007019
Alexis Tyler <xo@wvvw.me>
2021-12-17 14:44:26 +1030

    refactor: display-single-post

--- routes/display-single-post.js
+++ routes/display-single-post.js
@@ -3,194 +3,137 @@ const express = require('express')
 const db = require('../db')
 const myMisc = require('../misc.js')
 
-const router = express.Router({mergeParams: true})
+// Create router
+const router = express.Router({
+    mergeParams: true
+});
 
-//single post
-router.route('/')
-    .get(async (req, res) => {
-        const postPublicId = req.params[0]
-        const finalUserId = req.session.user ? req.session.user.user_id : -1
-        const filterUserId = await db.getCurrEyesId(req)
+/**
+ * Render a single post by it's ID
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @returns {void | express.Response}
+ */
+const get = async (req, res) => {
+    const postPublicId = req.params[0];
+    const finalUserId = req.session.user ? req.session.user.user_id : -1;
+    const filterUserId = await db.getCurrEyesId(req);
 
-        const {rows} = await db.getPostWithPublic2(
-            postPublicId,
-            myMisc.getCurrTimeZone(req),
-            finalUserId,
-            filterUserId)
+    // Fetch the current post
+    const { rows: [row] } = await db.getPostWithPublic2(postPublicId, myMisc.getCurrTimeZone(req), finalUserId, filterUserId);
 
-        if(rows.length) {
+    // Bail if we didn't get a post back
+    if(!row) return res.send('not found');
 
-            //
-            const isAllowed = await db.isAllowedToViewPost(
-                rows[0].private_group_ids,
-                finalUserId)
+    // Render an error page if they're not allowed
+    const isAllowed = await db.isAllowedToViewPost(row.private_group_ids, finalUserId);
+    if (!isAllowed) {
+        return res.render('message', {
+            html_title: 'Post #' + row.public_id,
+            message: "This post is from a private group and you do not have access.",
+            user: req.session.user,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        });
+    }
 
-            if(!isAllowed) {
-                return res.render(
-                    'message',
-                    {
-                        html_title: 'Post #' + rows[0].public_id,
-                        message: "This post is from a private group and you do not have access.",
-                        user: req.session.user,
-                        max_width: myMisc.getCurrSiteMaxWidth(req)
-                    })
-            }
+    // Get the requested page or fall back to 1
+    const requestedPage = Array.isArray(req.query.p) ? req.query.p[0] : req.query.p;
+    const page = parseInt(requestedPage?.toString() ?? '1', 10);
 
-            //
-            let page = 1
+    // Check if discover mode is enabled
+    const isDiscoverModeEnabled = myMisc.isDiscover(req);
 
-            if(typeof req.query.p !== 'undefined') {
-                page = parseInt(req.query.p)
+    // Get the comments for this post
+    const { rows: comments } = await db.getPostComments(row.post_id, myMisc.getCurrTimeZone(req), finalUserId, isDiscoverModeEnabled, filterUserId, page);
 
-                if(isNaN(page)) {
-                    return res.redirect(`/p/${postPublicId}`)
-                }
-            }
+    // Set the title to depending on the user's permissions
+    const htmlTitle = row.is_visible ? row.title : 'Post #' + row.public_id;
 
-            //
-            const isDiscoverMode = myMisc.isDiscover(req)
+    // Render the post
+    res.render('single-post', {
+        html_title: htmlTitle,
+        user: req.session.user,
+        post: row,
+        comments,
+        errors: [],
+        is_discover_mode: isDiscoverModeEnabled,
+        comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
+        max_width: myMisc.getCurrSiteMaxWidth(req),
+        page
+    });
+};
 
-            const{rows:comments} = await db.getPostComments(
-                rows[0].post_id,
-                myMisc.getCurrTimeZone(req),
-                finalUserId,
-                isDiscoverMode,
-                filterUserId,
-                page)
+const post = async (req, res) => {
+    // Bail if the user isn't authenticated
+    // @todo: This should be middleware
+    if (!req.session.user) return res.send('nope...');
 
-            //
-            const htmlTitle = rows[0].is_visible
-                ? rows[0].title
-                : 'Post #' + rows[0].public_id;
+    const postPublicId = req.params[0];
+    const finalUserId = req.session.user ? req.session.user.user_id : -1;
+    const filterUserId = await db.getCurrEyesId(req);
 
-            //
-            res.render(
-                'single-post',
-                {
-                    html_title: htmlTitle,
-                    user: req.session.user,
-                    post: rows[0],
-                    comments: comments,
-                    errors: [],
-                    is_discover_mode: isDiscoverMode,
-                    comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
-                    max_width: myMisc.getCurrSiteMaxWidth(req),
-                    page: page
-                }
-            )
-        }
-        else {
-            res.send('not found')
-        }
-    })
-    .post(
-        async (req, res) => {
+    const { rows: [ row ] } = await db.getPostWithPublic2(postPublicId, myMisc.getCurrTimeZone(req), finalUserId, filterUserId);
 
-            if(req.session.user) {
-                const postPublicId = req.params[0]
-                const finalUserId = req.session.user ? req.session.user.user_id : -1
-                const filterUserId = await db.getCurrEyesId(req)
+    // Bail if no post was found
+    if (!row) return res.send('not found');
 
-                const {rows} = await db.getPostWithPublic2(
-                    postPublicId,
-                    myMisc.getCurrTimeZone(req),
-                    finalUserId,
-                    filterUserId)
+    // Render an error page if they're not allowed
+    const isAllowed = await db.isAllowedToViewPost(row.private_group_ids, req.session.user.user_id);
+    if(!isAllowed) {
+        return res.render('message', {
+            html_title: 'Post #' + row.public_id,
+            message: "This post is from a private group and you do not have access.",
+            user: req.session.user,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        });
+    }
 
-                if(rows.length) {
+    // Process the comment
+    const [compressedComment, errors] = myMisc.processComment(req.body.text_content);
 
-                    //
-                    const isAllowed = await db.isAllowedToViewPost(
-                        rows[0].private_group_ids,
-                        req.session.user.user_id)
+    // Render an error page if we found any errors in the comment
+    if (errors.length > 0) {
+        // Get the requested page or fall back to 1
+        const requestedPage = Array.isArray(req.query.p) ? req.query.p[0] : req.query.p;
+        const page = parseInt(requestedPage?.toString() ?? '1', 10);
 
-                    if(!isAllowed) {
-                        return res.render(
-                            'message',
-                            {
-                                html_title: 'Post #' + rows[0].public_id,
-                                message: "This post is from a private group and you do not have access.",
-                                user: req.session.user,
-                                max_width: myMisc.getCurrSiteMaxWidth(req)
-                            })
-                    }
+        // Check if discover mode is enabled
+        const isDiscoverModeEnabled = myMisc.isDiscover(req)
 
-                    let [compressedComment, errors] = myMisc.processComment(req.body.text_content)
+        const { rows: comments } = await db.getPostComments(row.post_id, myMisc.getCurrTimeZone(req), finalUserId, isDiscoverModeEnabled, filterUserId, page);
 
-                    //
-                    if(errors.length) {
+        // Set the title to depending on the user's permissions
+        const htmlTitle = row.is_visible ? row.title : 'Post #' + row.public_id;
 
-                        //
-                        let page = 1
+        // Render the post
+        return res.render('single-post', {
+            html_title: htmlTitle,
+            user: req.session.user,
+            post: row,
+            comments,
+            errors,
+            is_discover_mode: isDiscoverModeEnabled,
+            comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            page
+        });
+    }
 
-                        if(typeof req.query.p !== 'undefined') {
-                            page = parseInt(req.query.p)
+    // Save the comment to the database
+    const { rows:data1 } = await db.createPostComment(row.post_id, req.session.user.user_id, compressedComment);
 
-                            if(isNaN(page)) {
-                                return res.redirect(`/p/${postPublicId}`)
-                            }
-                        }
+    // Update the amount of comments on this post
+    await db.incPostNumComments(row.post_id);
 
-                        //
-                        const isDiscoverMode = myMisc.isDiscover(req)
+    // Redirect the user to the newly created comment
+    const numComments = row.num_comments + 1;
+    const pages = Math.ceil(numComments / config.commentsPerPage);
+    const redirectUrl = (pages > 1) ? `/p/${postPublicId}?p=${pages}#${data1[0].public_id}` : `/p/${postPublicId}#${data1[0].public_id}`;
+    return res.redirect(redirectUrl);
+};
 
-                        const{rows:comments} = await db.getPostComments(
-                            rows[0].post_id,
-                            myMisc.getCurrTimeZone(req),
-                            finalUserId,
-                            isDiscoverMode,
-                            filterUserId,
-                            page)
+// single post
+router.get('/', get);
+router.post('/', post);
 
-                        //
-                        const htmlTitle = rows[0].is_visible
-                            ? rows[0].title
-                            : 'Post #' + rows[0].public_id;
-
-                        //
-                        res.render(
-                            'single-post',
-                            {
-                                html_title: htmlTitle,
-                                user: req.session.user,
-                                post: rows[0],
-                                comments: comments,
-                                errors: errors,
-                                is_discover_mode: isDiscoverMode,
-                                comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
-                                max_width: myMisc.getCurrSiteMaxWidth(req),
-                                page: page
-                            }
-                        )
-                    }
-                    else {
-
-                        //
-                        const {rows:data1} = await db.createPostComment(
-                            rows[0].post_id,
-                            req.session.user.user_id,
-                            compressedComment)
-
-                        //
-                        await db.incPostNumComments(rows[0].post_id)
-
-                        //
-                        const numComments = rows[0].num_comments + 1
-                        const pages = Math.ceil(numComments/config.commentsPerPage)
-                        const redirectUrl = (pages > 1)
-                            ? `/p/${postPublicId}?p=${pages}#${data1[0].public_id}`
-                            : `/p/${postPublicId}#${data1[0].public_id}`
-
-                        return res.redirect(redirectUrl)
-                    }
-                }
-                else {
-                    res.send('not found')
-                }
-            }
-            else {
-                res.send('nope...')
-            }
-        })
-
-module.exports = router
+module.exports = router;
