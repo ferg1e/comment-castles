f1e11334b200d2b90cfeec9c6dbf71d78416d378
Ry Ferguson <rytf12@gmail.com>
2021-12-21 10:39:07 +0000

    single post: change some whitespace styling and rename a variable

--- routes/display-single-post.js
+++ routes/display-single-post.js
@@ -15,14 +15,19 @@ const get = async (req, res) => {
     const filterUserId = await db.getCurrEyesId(req)
 
     //
-    const { rows: [row] } = await db.getPostWithPublic2(postPublicId, myMisc.getCurrTimeZone(req), finalUserId, filterUserId)
+    const {rows:[row]} = await db.getPostWithPublic2(
+        postPublicId,
+        myMisc.getCurrTimeZone(req),
+        finalUserId,
+        filterUserId)
 
     //
     if(!row) return res.send('not found')
 
     //
     const isAllowed = await db.isAllowedToViewPost(row.private_group_ids, finalUserId)
-    if (!isAllowed) {
+
+    if(!isAllowed) {
         return res.render('message', {
             html_title: 'Post #' + row.public_id,
             message: "This post is from a private group and you do not have access.",
@@ -43,11 +48,19 @@ const get = async (req, res) => {
     }
 
     //
-    const isDiscoverModeEnabled = myMisc.isDiscover(req)
+    const isDiscoverMode = myMisc.isDiscover(req)
 
-    const { rows: comments } = await db.getPostComments(row.post_id, myMisc.getCurrTimeZone(req), finalUserId, isDiscoverModeEnabled, filterUserId, page)
+    const {rows:comments} = await db.getPostComments(
+        row.post_id,
+        myMisc.getCurrTimeZone(req),
+        finalUserId,
+        isDiscoverMode,
+        filterUserId,
+        page)
 
-    const htmlTitle = row.is_visible ? row.title : 'Post #' + row.public_id
+    const htmlTitle = row.is_visible
+        ? row.title
+        : 'Post #' + row.public_id
 
     //
     res.render('single-post', {
@@ -56,30 +69,36 @@ const get = async (req, res) => {
         post: row,
         comments,
         errors: [],
-        is_discover_mode: isDiscoverModeEnabled,
+        is_discover_mode: isDiscoverMode,
         comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
         max_width: myMisc.getCurrSiteMaxWidth(req),
         page
     })
-};
+}
 
+//
 const post = async (req, res) => {
 
     // @todo: This should be middleware
-    if (!req.session.user) return res.send('nope...')
+    if(!req.session.user) return res.send('nope...')
 
     //
     const postPublicId = req.params[0]
     const finalUserId = req.session.user ? req.session.user.user_id : -1
     const filterUserId = await db.getCurrEyesId(req)
 
-    const { rows: [ row ] } = await db.getPostWithPublic2(postPublicId, myMisc.getCurrTimeZone(req), finalUserId, filterUserId)
+    const {rows:[row]} = await db.getPostWithPublic2(
+        postPublicId,
+        myMisc.getCurrTimeZone(req),
+        finalUserId,
+        filterUserId)
 
     //
-    if (!row) return res.send('not found')
+    if(!row) return res.send('not found')
 
     //
     const isAllowed = await db.isAllowedToViewPost(row.private_group_ids, req.session.user.user_id)
+
     if(!isAllowed) {
         return res.render('message', {
             html_title: 'Post #' + row.public_id,
@@ -93,7 +112,7 @@ const post = async (req, res) => {
     const [compressedComment, errors] = myMisc.processComment(req.body.text_content)
 
     //
-    if (errors.length > 0) {
+    if(errors.length > 0) {
 
         //
         let page = 1
@@ -107,11 +126,19 @@ const post = async (req, res) => {
         }
 
         //
-        const isDiscoverModeEnabled = myMisc.isDiscover(req)
+        const isDiscoverMode = myMisc.isDiscover(req)
 
-        const { rows: comments } = await db.getPostComments(row.post_id, myMisc.getCurrTimeZone(req), finalUserId, isDiscoverModeEnabled, filterUserId, page)
+        const {rows:comments} = await db.getPostComments(
+            row.post_id,
+            myMisc.getCurrTimeZone(req),
+            finalUserId,
+            isDiscoverMode,
+            filterUserId,
+            page)
 
-        const htmlTitle = row.is_visible ? row.title : 'Post #' + row.public_id
+        const htmlTitle = row.is_visible
+            ? row.title
+            : 'Post #' + row.public_id
 
         //
         return res.render('single-post', {
@@ -120,7 +147,7 @@ const post = async (req, res) => {
             post: row,
             comments,
             errors,
-            is_discover_mode: isDiscoverModeEnabled,
+            is_discover_mode: isDiscoverMode,
             comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
             max_width: myMisc.getCurrSiteMaxWidth(req),
             page
@@ -128,7 +155,10 @@ const post = async (req, res) => {
     }
 
     //
-    const { rows:data1 } = await db.createPostComment(row.post_id, req.session.user.user_id, compressedComment)
+    const {rows:data1} = await db.createPostComment(
+        row.post_id,
+        req.session.user.user_id,
+        compressedComment)
 
     //
     await db.incPostNumComments(row.post_id)
@@ -136,12 +166,14 @@ const post = async (req, res) => {
     //
     const numComments = row.num_comments + 1
     const pages = Math.ceil(numComments / config.commentsPerPage)
-    const redirectUrl = (pages > 1) ? `/p/${postPublicId}?p=${pages}#${data1[0].public_id}` : `/p/${postPublicId}#${data1[0].public_id}`
+    const redirectUrl = (pages > 1)
+        ? `/p/${postPublicId}?p=${pages}#${data1[0].public_id}`
+        : `/p/${postPublicId}#${data1[0].public_id}`
+
     return res.redirect(redirectUrl)
 }
 
 //
 router.get('/', get)
 router.post('/', post)
-
 module.exports = router
