1052178b3be0219b9a0f613ee50c67382d805cc3
Ry Ferguson <rytf12@gmail.com>
2023-04-18 18:55:12 +0100

    oauth: do the minimum required in order for the oauth2-server authorize() call to redirect the user back to the originating app. This involves setting getClient(), saveAuthorizationCode() and getAccessToken() on the model; setting the authenticateHandler() on the options argument passed to authorize(); calling redirect when the authorize() promise resolves; and passing client_id, state, response_type and redirect_uri in the URL.

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,15 +1,61 @@
 const express = require('express')
+const OAuth2Server = require('oauth2-server')
+const Request = require('oauth2-server').Request
+const Response = require('oauth2-server').Response
 const db = require('../db')
 const myMisc = require('../misc.js')
 const config = require('../config')
 
 const router = express.Router()
-const htmlTitleHome = "title"
+
+//
+const oauth = new OAuth2Server({
+    model: {
+        getClient: (clientId, clientSecret) => {
+            return {
+                id: "mybigfakeid",
+                redirectUris: ['http://localhost:6007'],
+                grants: ['authorization_code'],
+            }
+        },
+
+        saveAuthorizationCode: (code, client, user) => {
+            return {
+                authorizationCode: code.authorizationCode,
+            }
+        },
+
+        getAccessToken: (accessToken) => {
+            return {
+                accessToken: accessToken,
+            }
+        }
+    }
+})
 
 //
 router.route('/authorize')
     .get(async (req, res) => {
-        res.send('authorize')        
+
+        //
+        const request = new Request(req);
+        const response = new Response(res);
+        const options = {
+            authenticateHandler: {
+                handle: (req, res) => {
+                    return req.session.user
+                }
+            }
+        }
+
+        //
+        oauth.authorize(request, response, options)
+            .then((code) => {
+                return res.redirect(response.headers.location)
+            })
+            .catch((error) => {
+                return res.send(error)
+            })
     })
 
 module.exports = router
