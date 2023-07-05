f13741908da0d2af67137f409f719cd31e4f30b9
Ry Ferguson <rytf12@gmail.com>
2023-07-05 20:33:18 +0100

    implement api edit post endpoint at PUT /post

--- routes/api.js
+++ routes/api.js
@@ -234,7 +234,88 @@ router.post(
 router.put(
     '/post',
     async (req, res) => {
-        return res.json([1,2,4,8,16])
+
+        //
+        const oauthData = await oauthAuthenticate(req, res)
+
+        //
+        if(!oauthData) {
+            return res.status(401).json({
+                errors: ['invalid or no user auth'],
+            })
+        }
+
+        //
+        if(typeof req.body.post_id === 'undefined') {
+            return res.status(400).json({
+                errors: ['no post_id in body'],
+            })
+        }
+
+        //
+        const postPublicId = req.body.post_id
+        const {rows:[row]} = await db.getPostWithPublic(postPublicId)
+
+        //
+        if(!row) {
+            return res.status(404).json({
+                errors: ["no post with that postid"],
+            })
+        }
+
+        //
+        if(row.user_id != oauthData.user.user_id) {
+            return res.status(403).json({
+                errors: ["wrong user"],
+            })
+        }
+
+        //
+        const fTitle = typeof req.body.title === 'undefined'
+            ? row.title
+            : req.body.title
+
+        //
+        const fLink = typeof req.body.link === 'undefined'
+            ? (row.link === null ? '' : row.link)
+            : req.body.link
+
+        //
+        const fTextContent = typeof req.body.text_content === 'undefined'
+            ? (row.text_content === null ? '' : row.text_content)
+            : req.body.text_content
+
+        //
+        const fTags = typeof req.body.tags === 'undefined'
+            ? row.tags.join(',')
+            : req.body.tags
+
+        //
+        const [errors, wsCompressedTitle, trimTags] = await db.validateEditPost(
+            fTitle,
+            fLink,
+            fTags,
+            row.private_group_names)
+
+        //
+        if(errors.length) {
+            return res.status(400).json({
+                errors: errors,
+            })
+        }
+
+        //
+        await db.updatePost(
+            row.post_id,
+            wsCompressedTitle,
+            fTextContent,
+            fLink,
+            trimTags)
+
+        //
+        return res.json({
+            post_id: postPublicId,
+        })
     }
 )
 
