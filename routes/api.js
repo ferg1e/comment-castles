3cfa6578f6a44b497884abd4389b6d23caccf56d
Ry Ferguson <rytf12@gmail.com>
2023-07-16 23:44:37 +0100

    api: implement the delete post API endpoint at DELETE /post

--- routes/api.js
+++ routes/api.js
@@ -319,6 +319,56 @@ router.put(
     }
 )
 
+// delete post
+router.delete(
+    '/post',
+    async (req, res) => {
+
+        //
+        const oauthData = await oauthAuthenticate(req, res)
+
+        //
+        if(!oauthData) {
+            return res.status(401).json({
+                errors: ['invalid or no user auth'],
+            })
+        }
+
+        //
+        if(typeof req.query.post_id === 'undefined') {
+            return res.status(400).json({
+                errors: ['no post_id in URL'],
+            })
+        }
+
+        //
+        const postPublicId = req.query.post_id
+        const {rows:[row]} = await db.getPostWithPublic(postPublicId)
+
+        //
+        if(!row) {
+            return res.status(404).json({
+                errors: ["no post with that post id"],
+            })
+        }
+
+        //
+        if(row.user_id != oauthData.user.user_id) {
+            return res.status(403).json({
+                errors: ["wrong user"],
+            })
+        }
+
+        //
+        db.deleteWholePost(row.post_id)
+
+        //
+        return res.json({
+            success: true,
+        })
+    }
+)
+
 //
 router.get(
     '/comment',
