cb7d69dffa99070b70a977814160c96f490d61ad
Ry Ferguson <rytf12@gmail.com>
2023-07-17 19:30:57 +0100

    api: build 'delete comment' API endpoint

--- routes/api.js
+++ routes/api.js
@@ -690,6 +690,56 @@ router.put(
     }
 )
 
+// delete comment
+router.delete(
+    '/comment',
+    async (req, res) => {
+
+        //
+        const oauthData = await oauthAuthenticate(req, res)
+
+        //
+        if(!oauthData) {
+            return res.status(401).json({
+                errors: ['invalid or no user auth'],
+            })
+        }
+
+        //
+        if(typeof req.query.comment_id === 'undefined') {
+            return res.status(400).json({
+                errors: ['no comment_id in URL'],
+            })
+        }
+
+        //
+        const commentPublicId = req.query.comment_id
+        const {rows:[row]} = await db.getCommentWithPublic(commentPublicId)
+
+        //
+        if(!row) {
+            return res.status(404).json({
+                errors: ["no comment with that comment id"],
+            })
+        }
+
+        //
+        if(row.user_id != oauthData.user.user_id) {
+            return res.status(403).json({
+                errors: ["wrong user"],
+            })
+        }
+
+        //
+        await db.deleteComment(row.path)
+
+        //
+        return res.json({
+            success: true,
+        })
+    }
+)
+
 //
 module.exports = router
 
