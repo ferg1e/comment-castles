198112386dcf7cc08aebba0fe28948c24b114f60
Ry Ferguson <rytf12@gmail.com>
2023-09-07 16:05:18 +0100

    user profile page: add a follow/unfollow button; show/hide the profile blurb based on if the logged in user is following

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -27,6 +27,24 @@ const get = async (req, res) => {
     }
 
     //
+    const loggedInUserId = req.session.user ? req.session.user.user_id : -1
+    const filterUserId = await db.getCurrEyesId(req)
+    const targetUserId = dbUser.user_id
+    let isFollow = false
+
+    //
+    if(loggedInUserId != -1) {
+        const {rows:[flag]} = await db.getUserFollowee(loggedInUserId, targetUserId)
+        isFollow = typeof flag != 'undefined'
+    }
+
+    //
+    const {rows:[flag2]} = await db.getUserFollowee(filterUserId, targetUserId)
+    const isVisible = loggedInUserId == targetUserId ||
+        filterUserId == targetUserId ||
+        typeof flag2 != 'undefined'
+
+    //
     return res.render(
         'user-profile',
         {
@@ -35,6 +53,10 @@ const get = async (req, res) => {
             max_width: myMisc.getCurrSiteMaxWidth(req),
             username: dbUser.username,
             profile_text: dbUser.profile_blurb,
+            is_follow: isFollow,
+            is_visible: isVisible,
+            is_self: loggedInUserId == targetUserId,
+            user_public_id: userPublicId,
         }
     )
 }
