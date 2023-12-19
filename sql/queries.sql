8b149bd5d5708fa9bf858f64ea3664b38030a829
Ry Ferguson <rytf12@gmail.com>
2023-12-19 15:50:28 +0000

    use correct comment database table name in queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -2,17 +2,17 @@
 update
     tpost p
 set
-    num_comments = (select count(1) from ttest where path <@ text2ltree(p.post_id::text));
+    num_comments = (select count(1) from tcomment where path <@ text2ltree(p.post_id::text));
 
 --list num_comments for all posts
 select
     p.post_id,
     p.title,
-    (select count(1) from ttest where path <@ text2ltree(p.post_id::text)) num_comments
+    (select count(1) from tcomment where path <@ text2ltree(p.post_id::text)) num_comments
 from
     tpost p
 order by
     p.post_id;
 
 -- clear/truncate all tables and reset "autoincrement"
-truncate tfollower, tpost, tposttag, ttag, ttest, tuser restart identity;
+truncate tfollower, tpost, tposttag, ttag, tcomment, tuser restart identity;
