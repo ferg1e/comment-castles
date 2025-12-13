b9eff1df1027083b4810c5d036ae941b970b27ef
Ry Ferguson <rytf12@gmail.com>
2025-12-13 17:42:27 +0000

    refactor home page router: use standard project router structure

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,44 +7,45 @@ const router = express.Router()
 const htmlTitleHome = config.siteName
 
 //
-router.route('/')
-    .get(async (req, res) => {
-        
-        //
-        let page = 1
+const getHome = async (req, res) => {
 
-        if(typeof req.query.p !== 'undefined') {
-            page = parseInt(req.query.p)
+    //
+    let page = 1
 
-            if(isNaN(page)) {
-                return res.redirect('/')
-            }
+    if(typeof req.query.p !== 'undefined') {
+        page = parseInt(req.query.p)
+
+        if(isNaN(page)) {
+            return res.redirect('/')
         }
+    }
 
-        //
-        const sort = myMisc.getPostSort(req)
+    //
+    const sort = myMisc.getPostSort(req)
 
-        //
-        const {rows} = await db.getPosts(
-            myMisc.getCurrTimeZone(req),
-            page,
-            sort,
-            myMisc.getCurrPostsPerPage(req),
-            myMisc.getCurrDateFormat(req))
+    //
+    const {rows} = await db.getPosts(
+        myMisc.getCurrTimeZone(req),
+        page,
+        sort,
+        myMisc.getCurrPostsPerPage(req),
+        myMisc.getCurrDateFormat(req))
 
-        res.render(
-            'posts2',
-            {
-                html_title: htmlTitleHome,
-                user: req.session.user,
-                posts: rows,
-                page: page,
-                base_url: '/',
-                max_width: myMisc.getCurrSiteMaxWidth(req),
-                post_layout: myMisc.getCurrPostLayout(req),
-                sort: sort,
-                posts_vertical_spacing: myMisc.getCurrPostsVerticalSpacing(req),
-            })
-    })
+    res.render(
+        'posts2',
+        {
+            html_title: htmlTitleHome,
+            user: req.session.user,
+            posts: rows,
+            page: page,
+            base_url: '/',
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            post_layout: myMisc.getCurrPostLayout(req),
+            sort: sort,
+            posts_vertical_spacing: myMisc.getCurrPostsVerticalSpacing(req),
+        })
+}
 
+//
+router.get('/', getHome)
 module.exports = router
