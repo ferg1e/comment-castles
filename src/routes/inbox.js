3f9c6fbc942d9b427d51c3ab22b130b6122659bc
Ry Ferguson <rytf12@gmail.com>
2025-12-17 17:05:17 +0000

    refactor inbox router: use standard router function structure

--- src/routes/inbox.js
+++ src/routes/inbox.js
@@ -4,52 +4,54 @@ const myMisc = require('../util/misc.js')
 
 const router = express.Router()
 
-router.route('/')
-    .get(async (req, res) => {
-        if(req.session.user) {
+//
+const get = async (req, res) => {
+    if(req.session.user) {
 
-            //
-            let page = 1
+        //
+        let page = 1
 
-            if(typeof req.query.p !== 'undefined') {
-                page = parseInt(req.query.p)
+        if(typeof req.query.p !== 'undefined') {
+            page = parseInt(req.query.p)
 
-                if(isNaN(page)) {
-                    return res.redirect('/inbox')
-                }
+            if(isNaN(page)) {
+                return res.redirect('/inbox')
             }
-
-            //
-            const{rows:comments} = await db.getInboxComments(
-                myMisc.getCurrTimeZone(req),
-                req.session.user.user_id,
-                page,
-                myMisc.getCurrDateFormat(req))
-
-            //
-            res.render(
-                'inbox',
-                {
-                    html_title: 'Inbox',
-                    user: req.session.user,
-                    comments: comments,
-                    page: page,
-                    comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
-                    max_width: myMisc.getCurrSiteMaxWidth(req)
-                }
-            )
         }
-        else {
-            res.render(
-                'message',
-                {
-                    html_title: 'Inbox',
-                    message: "<a href=\"/login\">Log in</a> to view your inbox.",
-                    user: req.session.user,
-                    max_width: myMisc.getCurrSiteMaxWidth(req),
-                    main_class: 'main-text'
-                })
-        }
-    })
 
+        //
+        const{rows:comments} = await db.getInboxComments(
+            myMisc.getCurrTimeZone(req),
+            req.session.user.user_id,
+            page,
+            myMisc.getCurrDateFormat(req))
+
+        //
+        res.render(
+            'inbox',
+            {
+                html_title: 'Inbox',
+                user: req.session.user,
+                comments: comments,
+                page: page,
+                comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
+                max_width: myMisc.getCurrSiteMaxWidth(req)
+            }
+        )
+    }
+    else {
+        res.render(
+            'message',
+            {
+                html_title: 'Inbox',
+                message: "<a href=\"/login\">Log in</a> to view your inbox.",
+                user: req.session.user,
+                max_width: myMisc.getCurrSiteMaxWidth(req),
+                main_class: 'main-text'
+            })
+    }
+}
+
+//
+router.get('/', get)
 module.exports = router
