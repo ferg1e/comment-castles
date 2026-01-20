c3eda0e2e73b73b9d0666004c4dad041eca1d0e5
Ry Ferguson <rytf12@gmail.com>
2026-01-20 22:52:40 +0000

    single comment page POST: replace dbComment if else with early returning if

--- src/routes/single-comment-display.js
+++ src/routes/single-comment-display.js
@@ -78,79 +78,78 @@ const post = async (req, res) => {
         myMisc.getCurrTimeZone(req),
         myMisc.getCurrDateFormat(req))
 
-    if(dbComment) {
+    //
+    if(!dbComment) {
+        return res.send('not found')
+    }
+
+    //
+    let [compressedComment, errors] = myMisc.processComment(req.body.text_content)
+
+    if(errors.length) {
 
         //
-        let [compressedComment, errors] = myMisc.processComment(req.body.text_content)
+        let page = 1
 
-        if(errors.length) {
+        if(typeof req.query.p !== 'undefined') {
+            page = parseInt(req.query.p)
 
-            //
-            let page = 1
-
-            if(typeof req.query.p !== 'undefined') {
-                page = parseInt(req.query.p)
-
-                if(isNaN(page)) {
-                    return res.redirect(`/c/${commentPublicId}`)
-                }
+            if(isNaN(page)) {
+                return res.redirect(`/c/${commentPublicId}`)
             }
+        }
 
-            const{rows:comments} = await db.getCommentComments(
-                dbComment.path,
-                myMisc.getCurrTimeZone(req),
+        const{rows:comments} = await db.getCommentComments(
+            dbComment.path,
+            myMisc.getCurrTimeZone(req),
+            page,
+            myMisc.getCurrDateFormat(req))
+
+        //
+        const {rows:[{count:numComments}]} = await db.getCommentNumComments(dbComment.path)
+        const totalPages = Math.ceil(numComments/config.commentsPerPage)
+
+        //
+        res.render(
+            'single-comment',
+            {
+                html_title: htmlTitleComment + commentPublicId,
+                user: req.session.user,
+                post_public_id: dbComment.post_public_id,
+                comment: dbComment,
+                comments: comments,
+                errors: errors,
+                comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
+                max_width: myMisc.getCurrSiteMaxWidth(req),
                 page,
-                myMisc.getCurrDateFormat(req))
-
-            //
-            const {rows:[{count:numComments}]} = await db.getCommentNumComments(dbComment.path)
-            const totalPages = Math.ceil(numComments/config.commentsPerPage)
-
-            //
-            res.render(
-                'single-comment',
-                {
-                    html_title: htmlTitleComment + commentPublicId,
-                    user: req.session.user,
-                    post_public_id: dbComment.post_public_id,
-                    comment: dbComment,
-                    comments: comments,
-                    errors: errors,
-                    comment_reply_mode: myMisc.getCurrCommentReplyMode(req),
-                    max_width: myMisc.getCurrSiteMaxWidth(req),
-                    page,
-                    total_pages: totalPages,
-                    lead_mod_user_id: dbComment.lead_mod,
-                    curr_castle: dbComment.castle,
-                }
-            )
-        }
-        else {
-
-            //
-            const {rows:data1} = await db.createCommentComment(
-                dbComment.post_id,
-                req.session.user.user_id,
-                compressedComment,
-                dbComment.path,
-                config.defaultTimeZone,
-                config.defaultDateFormat,
-                dbComment.user_id)
-
-            //
-            const {rows:data2} = await db.getCommentNumComments(dbComment.path)
-
-            const numComments = data2[0]['count']
-            const pages = Math.ceil(numComments/config.commentsPerPage)
-            const redirectUrl = (pages > 1)
-                ? `/c/${commentPublicId}?p=${pages}#${data1[0].public_id}`
-                : `/c/${commentPublicId}#${data1[0].public_id}`
-
-            return res.redirect(redirectUrl)
-        }
+                total_pages: totalPages,
+                lead_mod_user_id: dbComment.lead_mod,
+                curr_castle: dbComment.castle,
+            }
+        )
     }
     else {
-        res.send('not found')
+
+        //
+        const {rows:data1} = await db.createCommentComment(
+            dbComment.post_id,
+            req.session.user.user_id,
+            compressedComment,
+            dbComment.path,
+            config.defaultTimeZone,
+            config.defaultDateFormat,
+            dbComment.user_id)
+
+        //
+        const {rows:data2} = await db.getCommentNumComments(dbComment.path)
+
+        const numComments = data2[0]['count']
+        const pages = Math.ceil(numComments/config.commentsPerPage)
+        const redirectUrl = (pages > 1)
+            ? `/c/${commentPublicId}?p=${pages}#${data1[0].public_id}`
+            : `/c/${commentPublicId}#${data1[0].public_id}`
+
+        return res.redirect(redirectUrl)
     }
 }
 
