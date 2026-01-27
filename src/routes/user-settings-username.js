bf2c4fc8db5fbe5edd467fa1d50a382cf8b98850
Ry Ferguson <rytf12@gmail.com>
2026-01-27 17:14:23 +0000

    change username page: use standard project route function structure

--- src/routes/user-settings-username.js
+++ src/routes/user-settings-username.js
@@ -7,87 +7,92 @@ const router = express.Router()
 const regexUsername = /^[a-z0-9-]{4,16}$/i
 const htmlTitleSettingsUsername = 'Settings / Username'
 
-router.route('/')
-    .get(async (req, res) => {
+//
+const get = async (req, res) => {
+
+    //
+    if(!req.session.user) {
+        return res.redirect('/settings')
+    }
+
+    //
+    res.render(
+        'my-settings-username',
+        {
+            html_title: htmlTitleSettingsUsername,
+            user: req.session.user,
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            errors: [],
+            username: req.session.user.username
+        })
+}
+
+//
+const post = async (req, res) => {
+
+    if(req.session.user) {
 
         //
-        if(!req.session.user) {
-            return res.redirect('/settings')
-        }
+        const errors = []
+        const username = req.body.username
 
         //
-        res.render(
-            'my-settings-username',
-            {
-                html_title: htmlTitleSettingsUsername,
-                user: req.session.user,
-                max_width: myMisc.getCurrSiteMaxWidth(req),
-                errors: [],
-                username: req.session.user.username
-            })
-    })
-    .post(async (req, res) => {
-
-        if(req.session.user) {
-
-            //
-            const errors = []
-            const username = req.body.username
-
-            //
-            if(!username.match(regexUsername)) {
-                errors.push({msg: "Username must be 4-16 characters(letters, numbers and dashes only)"})
-            }
-            else {
-                const {rows} = await db.getUserWithUsername(username)
-
-                if(rows.length && rows[0].user_id != req.session.user.user_id) {
-                    errors.push({msg: "username already taken"})
-                }
-            }
-
-            //
-            const {rows:rows2} = await db.getUserWithUserId(req.session.user.user_id)
-            const isCorrectPassword = await argon2.verify(rows2[0].password, req.body.password)
-
-            if(!isCorrectPassword) {
-                errors.push({msg: "incorrect existing password"})
-            }
-
-            //
-            if(errors.length) {
-                res.render(
-                    'my-settings-username',
-                    {
-                        html_title: htmlTitleSettingsUsername,
-                        user: req.session.user,
-                        max_width: myMisc.getCurrSiteMaxWidth(req),
-                        errors: errors,
-                        username: username
-                    })
-            }
-            else {
-
-                //
-                await db.updateUserUsername(req.session.user.user_id, username)
-                req.session.user.username = username
-
-                //
-                res.render(
-                    'my-settings-username',
-                    {
-                        html_title: htmlTitleSettingsUsername,
-                        user: req.session.user,
-                        max_width: myMisc.getCurrSiteMaxWidth(req),
-                        errors: [],
-                        success: 'Username successfully saved',
-                        username: username
-                    })
-            }
+        if(!username.match(regexUsername)) {
+            errors.push({msg: "Username must be 4-16 characters(letters, numbers and dashes only)"})
         }
         else {
-            res.send('nope...')
-        }
-    })
+            const {rows} = await db.getUserWithUsername(username)
 
+            if(rows.length && rows[0].user_id != req.session.user.user_id) {
+                errors.push({msg: "username already taken"})
+            }
+        }
+
+        //
+        const {rows:rows2} = await db.getUserWithUserId(req.session.user.user_id)
+        const isCorrectPassword = await argon2.verify(rows2[0].password, req.body.password)
+
+        if(!isCorrectPassword) {
+            errors.push({msg: "incorrect existing password"})
+        }
+
+        //
+        if(errors.length) {
+            res.render(
+                'my-settings-username',
+                {
+                    html_title: htmlTitleSettingsUsername,
+                    user: req.session.user,
+                    max_width: myMisc.getCurrSiteMaxWidth(req),
+                    errors: errors,
+                    username: username
+                })
+        }
+        else {
+
+            //
+            await db.updateUserUsername(req.session.user.user_id, username)
+            req.session.user.username = username
+
+            //
+            res.render(
+                'my-settings-username',
+                {
+                    html_title: htmlTitleSettingsUsername,
+                    user: req.session.user,
+                    max_width: myMisc.getCurrSiteMaxWidth(req),
+                    errors: [],
+                    success: 'Username successfully saved',
+                    username: username
+                })
+        }
+    }
+    else {
+        res.send('nope...')
+    }
+}
+
+//
+router.get('/', get)
+router.post('/', post)
 module.exports = router
