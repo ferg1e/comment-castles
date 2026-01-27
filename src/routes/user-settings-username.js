8bd5453002145290b7ec25d7351e62ef62581e6e
Ry Ferguson <rytf12@gmail.com>
2026-01-27 18:29:04 +0000

    change username page: replace user session if else with an early returning if in order to reduce indentation levels

--- src/routes/user-settings-username.js
+++ src/routes/user-settings-username.js
@@ -28,61 +28,60 @@ const get = async (req, res) => {
 //
 const post = async (req, res) => {
 
-    if(req.session.user) {
+    //
+    if(!req.session.user) {
+        return res.send('nope...')
+    }
 
-        //
-        const errors = []
-        const username = req.body.username
+    //
+    const errors = []
+    const username = req.body.username
 
-        //
-        if(!username.match(regexUsername)) {
-            errors.push({msg: "Username must be 4-16 characters(letters, numbers and dashes only)"})
-        }
-        else {
-            const {rows} = await db.getUserWithUsername(username)
-
-            if(rows.length && rows[0].user_id != req.session.user.user_id) {
-                errors.push({msg: "username already taken"})
-            }
-        }
-
-        //
-        const {rows:rows2} = await db.getUserWithUserId(req.session.user.user_id)
-        const isCorrectPassword = await argon2.verify(rows2[0].password, req.body.password)
-
-        if(!isCorrectPassword) {
-            errors.push({msg: "incorrect existing password"})
-        }
-
-        //
-        if(errors.length) {
-            res.render('my-settings-username', {
-                html_title: htmlTitleSettingsUsername,
-                user: req.session.user,
-                max_width: myMisc.getCurrSiteMaxWidth(req),
-                errors: errors,
-                username: username
-            })
-        }
-        else {
-
-            //
-            await db.updateUserUsername(req.session.user.user_id, username)
-            req.session.user.username = username
-
-            //
-            res.render('my-settings-username', {
-                html_title: htmlTitleSettingsUsername,
-                user: req.session.user,
-                max_width: myMisc.getCurrSiteMaxWidth(req),
-                errors: [],
-                success: 'Username successfully saved',
-                username: username
-            })
-        }
+    //
+    if(!username.match(regexUsername)) {
+        errors.push({msg: "Username must be 4-16 characters(letters, numbers and dashes only)"})
     }
     else {
-        res.send('nope...')
+        const {rows} = await db.getUserWithUsername(username)
+
+        if(rows.length && rows[0].user_id != req.session.user.user_id) {
+            errors.push({msg: "username already taken"})
+        }
+    }
+
+    //
+    const {rows:rows2} = await db.getUserWithUserId(req.session.user.user_id)
+    const isCorrectPassword = await argon2.verify(rows2[0].password, req.body.password)
+
+    if(!isCorrectPassword) {
+        errors.push({msg: "incorrect existing password"})
+    }
+
+    //
+    if(errors.length) {
+        res.render('my-settings-username', {
+            html_title: htmlTitleSettingsUsername,
+            user: req.session.user,
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            errors: errors,
+            username: username
+        })
+    }
+    else {
+
+        //
+        await db.updateUserUsername(req.session.user.user_id, username)
+        req.session.user.username = username
+
+        //
+        res.render('my-settings-username', {
+            html_title: htmlTitleSettingsUsername,
+            user: req.session.user,
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            errors: [],
+            success: 'Username successfully saved',
+            username: username
+        })
     }
 }
 
