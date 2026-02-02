87745ac7a16b711f6042b8d066ceef813af9b6d4
Ry Ferguson <rytf12@gmail.com>
2026-02-02 22:07:12 +0000

    generalize redirect and link in sitePaginate() by using req.path instead of '/'

--- src/middleware/site-paginate.js
+++ src/middleware/site-paginate.js
@@ -13,13 +13,13 @@ function sitePaginate(req, res, next) {
         return res.status(400).render('http-error-400', {
             message: `Invalid pagination page value in URL. ` +
                 `This value must be an integer between 1 and ${Number.MAX_SAFE_INTEGER}. ` +
-                `<a href="/">Return to page 1</a>.`
+                `<a href="${req.path}">Return to page 1</a>.`
         })
     }
 
     // if p=1 supplied then redirect to no p in URL
     if(typeof req.query.p !== 'undefined' && page == 1) {
-        return res.redirect(301, '/')
+        return res.redirect(301, req.path)
     }
 
     //
