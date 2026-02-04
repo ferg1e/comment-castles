1cadc931212702733c4e4223d6af6865841d80f6
Ry Ferguson <rytf12@gmail.com>
2026-02-04 17:42:20 +0000

    fix base url in sitePageValue()

--- src/middleware/site-page-value.js
+++ src/middleware/site-page-value.js
@@ -4,6 +4,7 @@ const myMisc = require('../util/misc.js')
 function sitePageValue(req, res, next) {
 
     //
+    const fullPath = req.originalUrl.replace(/\?.*$/, '')
     let page
 
     try {
@@ -13,13 +14,13 @@ function sitePageValue(req, res, next) {
         return res.status(400).render('http-error-400', {
             message: `Invalid pagination page value in URL. ` +
                 `This value must be an integer between 1 and ${Number.MAX_SAFE_INTEGER}. ` +
-                `<a href="${req.path}">Return to page 1</a>.`
+                `<a href="${fullPath}">Return to page 1</a>.`
         })
     }
 
     // redirect if p=1 in url
     if(typeof req.query.p !== 'undefined' && page == 1) {
-        return res.redirect(301, req.path)
+        return res.redirect(301, fullPath)
     }
 
     //
