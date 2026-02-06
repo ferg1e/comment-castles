546e5dcaa8f7015ee64297906afed943b54bc72f
Ry Ferguson <rytf12@gmail.com>
2026-02-06 18:23:04 +0000

    hashtag posts page: use standard router function structure

--- src/routes/hashtag-posts.js
+++ src/routes/hashtag-posts.js
@@ -4,65 +4,65 @@ const myMisc = require('../util/misc.js')
 
 const router = express.Router({mergeParams: true})
 
-router.get(
-    '/',
-    async (req, res) => {
-        const hashtag = req.params[0]
+//
+const get = async (req, res) => {
+    const hashtag = req.params[0]
 
-        //
-        let page = 1
+    //
+    let page = 1
 
-        if(typeof req.query.p !== 'undefined') {
-            page = parseInt(req.query.p)
+    if(typeof req.query.p !== 'undefined') {
+        page = parseInt(req.query.p)
 
-            if(isNaN(page)) {
-                return res.redirect(`/t/${hashtag}`)
-            }
+        if(isNaN(page)) {
+            return res.redirect(`/t/${hashtag}`)
         }
-
-        //
-        const {rows:[dbHashtag]} = await db.getHashtag(hashtag)
-        const sort = myMisc.getPostSort(req)
-        let posts = []
-        let numPages = 0
-
-        //
-        if(dbHashtag) {
-
-            //
-            const postsPerPage = 3
-            const {rows:[{count:postsCount}]} = await db.getHashtagPostsCount(dbHashtag.hashtag_id)
-            numPages = Math.ceil(postsCount/postsPerPage)
-
-            //
-            const {rows} = await db.getHashtagPosts(
-                myMisc.getCurrTimeZone(req),
-                page,
-                dbHashtag.hashtag_id,
-                sort,
-                postsPerPage,
-                myMisc.getCurrDateFormat(req))
-
-            posts = rows
-        }
-
-        //
-        res.render(
-            'posts2',
-            {
-                html_title: `#${hashtag}`,
-                page_title: `#${hashtag}`,
-                user: req.session.user,
-                posts: posts,
-                page: page,
-                base_url: `/t/${hashtag}`,
-                max_width: myMisc.getCurrSiteMaxWidth(req),
-                post_layout: myMisc.getCurrPostLayout(req),
-                sort: sort,
-                posts_vertical_spacing: myMisc.getCurrPostsVerticalSpacing(req),
-                num_pages: numPages
-            })
     }
-)
 
+    //
+    const {rows:[dbHashtag]} = await db.getHashtag(hashtag)
+    const sort = myMisc.getPostSort(req)
+    let posts = []
+    let numPages = 0
+
+    //
+    if(dbHashtag) {
+
+        //
+        const postsPerPage = 3
+        const {rows:[{count:postsCount}]} = await db.getHashtagPostsCount(dbHashtag.hashtag_id)
+        numPages = Math.ceil(postsCount/postsPerPage)
+
+        //
+        const {rows} = await db.getHashtagPosts(
+            myMisc.getCurrTimeZone(req),
+            page,
+            dbHashtag.hashtag_id,
+            sort,
+            postsPerPage,
+            myMisc.getCurrDateFormat(req))
+
+        posts = rows
+    }
+
+    //
+    res.render(
+        'posts2',
+        {
+            html_title: `#${hashtag}`,
+            page_title: `#${hashtag}`,
+            user: req.session.user,
+            posts: posts,
+            page: page,
+            base_url: `/t/${hashtag}`,
+            max_width: myMisc.getCurrSiteMaxWidth(req),
+            post_layout: myMisc.getCurrPostLayout(req),
+            sort: sort,
+            posts_vertical_spacing: myMisc.getCurrPostsVerticalSpacing(req),
+            num_pages: numPages
+        })
+}
+
+//
+router.get('/', get)
 module.exports = router
