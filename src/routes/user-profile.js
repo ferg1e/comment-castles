1d8716c619165630691ab8ce21f12c4d2f0f1fcd
Ry Ferguson <rytf12@gmail.com>
2026-02-17 15:58:59 +0000

    user profile page: issue 404 when there is no user with that ID

--- src/routes/user-profile.js
+++ src/routes/user-profile.js
@@ -14,15 +14,10 @@ const get = async (req, res) => {
 
     //
     if(!dbUser) {
-        return res.render(
-            'message',
-            {
-                html_title: "Unknown User",
-                message: "No user with that ID.",
-                user: req.session.user,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            }
-        )
+        return res.status(404).render('http-error-404', {
+            message: `There is no user with that user ID. ` +
+                `<a href="/">Return to the home page</a>.`
+        })
     }
 
     //
