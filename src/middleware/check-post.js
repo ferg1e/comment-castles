b5ae56a6915cb57bd144a9ab585652e9cb1b7170
Ry Ferguson <rytf12@gmail.com>
2026-02-21 23:39:43 +0000

    create checkPost() middleware

---
+++ src/middleware/check-post.js
@@ -0,0 +1,22 @@
+const db = require('../db')
+
+//
+async function checkPost(req, res, next) {
+    const postPublicId = req.params[0]
+    const {rows:[post]} = await db.getPostWithPublic(postPublicId)
+
+    //
+    if(!post) {
+        return res.status(404).render('http-error-404', {
+            message: `There is no post with ID ${postPublicId}. ` +
+                `<a href="/">Return to the home page</a>.`
+        })
+    }
+
+    //
+    res.locals.post = post
+    next()
+}
+
+//
+module.exports.checkPost = checkPost
