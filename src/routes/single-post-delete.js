8a3e88cdb32596118b854dcac0336a898393c581
Ry Ferguson <rytf12@gmail.com>
2026-02-21 21:44:48 +0000

    post delete: use standard route function structure

--- src/routes/single-post-delete.js
+++ src/routes/single-post-delete.js
@@ -6,73 +6,78 @@ const config = require('../config')
 const router = express.Router({mergeParams: true})
 const htmlTitle = 'Delete Post'
 
-router.route('/')
-    .get(async (req, res) => {
+//
+const get = async (req, res) => {
 
-        //
-        if(!req.session.user) {
-            return res.send('sorry...')
-        }
+    //
+    if(!req.session.user) {
+        return res.send('sorry...')
+    }
 
-        //
-        const postPublicId = req.params[0]
-        const {rows} = await db.getPostWithPublic(postPublicId)
+    //
+    const postPublicId = req.params[0]
+    const {rows} = await db.getPostWithPublic(postPublicId)
 
-        //
-        if(!rows.length) {
-            return res.send('unknown post...')
-        }
+    //
+    if(!rows.length) {
+        return res.send('unknown post...')
+    }
 
-        //
-        if(!(rows[0].user_id == req.session.user.user_id || req.session.user.user_id == config.adminUserId || rows[0].lead_mod == req.session.user.user_id)) {
-            return res.send('wrong user...')
-        }
+    //
+    if(!(rows[0].user_id == req.session.user.user_id || req.session.user.user_id == config.adminUserId || rows[0].lead_mod == req.session.user.user_id)) {
+        return res.send('wrong user...')
+    }
 
-        //
-        res.render(
-            'delete-post',
-            {
-                html_title: htmlTitle,
-                user: req.session.user,
-                title: rows[0].title,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
-    })
-    .post(async (req, res) => {
+    //
+    res.render(
+        'delete-post',
+        {
+            html_title: htmlTitle,
+            user: req.session.user,
+            title: rows[0].title,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
+}
+
+//
+const post = async (req, res) => {
             
-        //
-        if(!req.session.user) {
-            return res.send('nope...')
-        }
+    //
+    if(!req.session.user) {
+        return res.send('nope...')
+    }
 
-        //
-        const postPublicId = req.params[0]
-        const {rows} = await db.getPostWithPublic(postPublicId)
+    //
+    const postPublicId = req.params[0]
+    const {rows} = await db.getPostWithPublic(postPublicId)
 
-        //
-        if(!rows.length) {
-            return res.send('unknown post...')
-        }
+    //
+    if(!rows.length) {
+        return res.send('unknown post...')
+    }
 
-        //
-        if(!(rows[0].user_id == req.session.user.user_id || req.session.user.user_id == config.adminUserId || rows[0].lead_mod == req.session.user.user_id)) {
-            return res.send('wrong user...')
-        }
+    //
+    if(!(rows[0].user_id == req.session.user.user_id || req.session.user.user_id == config.adminUserId || rows[0].lead_mod == req.session.user.user_id)) {
+        return res.send('wrong user...')
+    }
 
-        await db.deletePost(rows[0].post_id)
-        
-        return res.render(
-            'message',
-            {
-                html_title: htmlTitle,
-                message: "The post and all of its comments (if any) were successfully deleted.",
-                user: req.session.user,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
-    })
+    await db.deletePost(rows[0].post_id)
+    
+    return res.render(
+        'message',
+        {
+            html_title: htmlTitle,
+            message: "The post and all of its comments (if any) were successfully deleted.",
+            user: req.session.user,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
+}
 
+//
+router.get('/', get)
+router.post('/', post)
 module.exports = router
