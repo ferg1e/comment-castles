0ac78fa604d899da203399022eac33f03300ea46
Ry Ferguson <rytf12@gmail.com>
2026-02-24 22:35:50 +0000

    edit post: replace an if else with an early returning if in the GET route

--- src/routes/single-post-edit.js
+++ src/routes/single-post-edit.js
@@ -7,43 +7,44 @@ const htmlTitle = 'Edit Post'
 
 //
 const get = async (req, res) => {
-    if(req.session.user) {
 
-        //
-        const postPublicId = req.params[0]
-        const {rows} = await db.getPostWithPublic(postPublicId)
-
-        //
-        if(!rows.length) {
-            return res.send('unknown post...')
-        }
-
-        //
-        if(rows[0].user_id != req.session.user.user_id) {
-            return res.send('wrong user...')
-        }
-
-        //
-        res.render(
-            'new-post2',
-            {
-                html_title: htmlTitle,
-                user: req.session.user,
-                errors: [],
-                title: rows[0].title,
-                link: rows[0].link === null ? '' : rows[0].link,
-                textContent: rows[0].text_content,
-                castle: rows[0].castle,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                submitLabel: 'Edit Post',
-                heading: 'Edit Post',
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
+    //
+    if(!req.session.user) {
+        return res.send('nope...')
     }
-    else {
-        res.send('sorry...')
+
+    //
+    const postPublicId = req.params[0]
+    const {rows} = await db.getPostWithPublic(postPublicId)
+
+    //
+    if(!rows.length) {
+        return res.send('unknown post...')
     }
+
+    //
+    if(rows[0].user_id != req.session.user.user_id) {
+        return res.send('wrong user...')
+    }
+
+    //
+    res.render(
+        'new-post2',
+        {
+            html_title: htmlTitle,
+            user: req.session.user,
+            errors: [],
+            title: rows[0].title,
+            link: rows[0].link === null ? '' : rows[0].link,
+            textContent: rows[0].text_content,
+            castle: rows[0].castle,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            submitLabel: 'Edit Post',
+            heading: 'Edit Post',
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
+    
 }
 
 //
