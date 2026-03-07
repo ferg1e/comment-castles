6875a0ee7251c85f9b882544bfddea3d20ba9a24
Ry Ferguson <rytf12@gmail.com>
2026-03-07 23:09:01 +0000

    edit comment: use standard function structure for GET and POST routes

--- src/routes/single-comment-edit.js
+++ src/routes/single-comment-edit.js
@@ -5,88 +5,92 @@ const myMisc = require('../util/misc.js')
 const router = express.Router({mergeParams: true})
 const htmlTitle = 'Edit Comment'
 
-router.route('/')
-    .get(async (req, res) => {
-        if(req.session.user) {
+//
+const get = async (req, res) => {
+    if(req.session.user) {
 
-            //
-            const commentPublicId = req.params[0]
-            const {rows} = await db.getCommentWithPublic(commentPublicId)
+        //
+        const commentPublicId = req.params[0]
+        const {rows} = await db.getCommentWithPublic(commentPublicId)
 
-            //
-            if(!rows.length) {
-                return res.send('unknown comment...')
-            }
+        //
+        if(!rows.length) {
+            return res.send('unknown comment...')
+        }
 
-            //
-            if(rows[0].user_id != req.session.user.user_id) {
-                return res.send('wrong user...')
-            }
+        //
+        if(rows[0].user_id != req.session.user.user_id) {
+            return res.send('wrong user...')
+        }
 
-            //
+        //
+        res.render(
+            'edit-comment',
+            {
+                html_title: htmlTitle,
+                user: req.session.user,
+                errors: [],
+                textContent: rows[0].text_content,
+                lead_mod_user_id: rows[0].lead_mod,
+                curr_castle: rows[0].castle,
+                max_width: myMisc.getCurrSiteMaxWidth(req)
+            })
+    }
+    else {
+        res.send('sorry...')
+    }
+}
+
+//
+const post = async (req, res) => {
+    if(req.session.user) {
+
+        //
+        const commentPublicId = req.params[0]
+        const {rows} = await db.getCommentWithPublic(commentPublicId)
+
+        //
+        if(!rows.length) {
+            return res.send('unknown comment...')
+        }
+
+        //
+        if(rows[0].user_id != req.session.user.user_id) {
+            return res.send('wrong user...')
+        }
+
+        //
+        let [compressedComment, errors] = myMisc.processComment(req.body.text_content)
+
+        //
+        if(errors.length) {
             res.render(
                 'edit-comment',
                 {
                     html_title: htmlTitle,
                     user: req.session.user,
-                    errors: [],
-                    textContent: rows[0].text_content,
+                    errors: errors,
+                    textContent: "",
                     lead_mod_user_id: rows[0].lead_mod,
                     curr_castle: rows[0].castle,
                     max_width: myMisc.getCurrSiteMaxWidth(req)
                 })
         }
         else {
-            res.send('sorry...')
+            await db.updateComment(
+                rows[0].comment_id,
+                compressedComment)
+            
+            //
+            return res.redirect('/c/' + commentPublicId)
         }
-    })
-    .post(async (req, res) => {
-            if(req.session.user) {
+    }
+    else {
+        res.send('nope...')
+    }
+}
 
-                //
-                const commentPublicId = req.params[0]
-                const {rows} = await db.getCommentWithPublic(commentPublicId)
-
-                //
-                if(!rows.length) {
-                    return res.send('unknown comment...')
-                }
-
-                //
-                if(rows[0].user_id != req.session.user.user_id) {
-                    return res.send('wrong user...')
-                }
-
-                //
-                let [compressedComment, errors] = myMisc.processComment(req.body.text_content)
-
-                //
-                if(errors.length) {
-                    res.render(
-                        'edit-comment',
-                        {
-                            html_title: htmlTitle,
-                            user: req.session.user,
-                            errors: errors,
-                            textContent: "",
-                            lead_mod_user_id: rows[0].lead_mod,
-                            curr_castle: rows[0].castle,
-                            max_width: myMisc.getCurrSiteMaxWidth(req)
-                        })
-                }
-                else {
-                    await db.updateComment(
-                        rows[0].comment_id,
-                        compressedComment)
-                    
-                    //
-                    return res.redirect('/c/' + commentPublicId)
-                }
-            }
-            else {
-                res.send('nope...')
-            }
-        }
-    )
-
+//
+router.get('/', get)
+router.post('/', post)
 module.exports = router
