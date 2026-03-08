a082df8168c91c25c534a893b4b384144abc0fea
Ry Ferguson <rytf12@gmail.com>
2026-03-08 15:29:18 +0000

    edit comment: use isUser middleware on GET route

--- src/routes/single-comment-edit.js
+++ src/routes/single-comment-edit.js
@@ -1,44 +1,40 @@
 const express = require('express')
 const db = require('../db')
 const myMisc = require('../util/misc.js')
+const {isUser} = require('../middleware/is-user.js')
 
 const router = express.Router({mergeParams: true})
 const htmlTitle = 'Edit Comment'
 
 //
 const get = async (req, res) => {
-    if(req.session.user) {
+    
+    //
+    const commentPublicId = req.params[0]
+    const {rows} = await db.getCommentWithPublic(commentPublicId)
 
-        //
-        const commentPublicId = req.params[0]
-        const {rows} = await db.getCommentWithPublic(commentPublicId)
-
-        //
-        if(!rows.length) {
-            return res.send('unknown comment...')
-        }
-
-        //
-        if(rows[0].user_id != req.session.user.user_id) {
-            return res.send('wrong user...')
-        }
-
-        //
-        res.render(
-            'edit-comment',
-            {
-                html_title: htmlTitle,
-                user: req.session.user,
-                errors: [],
-                textContent: rows[0].text_content,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
+    //
+    if(!rows.length) {
+        return res.send('unknown comment...')
     }
-    else {
-        res.send('sorry...')
+
+    //
+    if(rows[0].user_id != req.session.user.user_id) {
+        return res.send('wrong user...')
     }
+
+    //
+    res.render(
+        'edit-comment',
+        {
+            html_title: htmlTitle,
+            user: req.session.user,
+            errors: [],
+            textContent: rows[0].text_content,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
 }
 
 //
@@ -91,6 +87,6 @@ const post = async (req, res) => {
 }
 
 //
-router.get('/', get)
+router.get('/', isUser, get)
 router.post('/', post)
 module.exports = router
