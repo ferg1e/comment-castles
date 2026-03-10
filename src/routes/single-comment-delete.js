59e3886e084aa7a6ccd68d5c9e409d8961164b06
Ry Ferguson <rytf12@gmail.com>
2026-03-10 17:49:44 +0000

    delete comment: use standard router function structure

--- src/routes/single-comment-delete.js
+++ src/routes/single-comment-delete.js
@@ -6,73 +6,78 @@ const config = require('../config')
 const router = express.Router({mergeParams: true})
 const htmlTitle = 'Delete Comment'
 
-router.route('/')
-    .get(async (req, res) => {
+//
+const get = async (req, res) => {
 
-        //
-        if(!req.session.user) {
-            return res.send('sorry...')
-        }
+    //
+    if(!req.session.user) {
+        return res.send('sorry...')
+    }
 
-        //
-        const commentPublicId = req.params[0]
-        const {rows} = await db.getCommentWithPublic(commentPublicId)
+    //
+    const commentPublicId = req.params[0]
+    const {rows} = await db.getCommentWithPublic(commentPublicId)
 
-        //
-        if(!rows.length) {
-            return res.send('unknown comment...')
-        }
+    //
+    if(!rows.length) {
+        return res.send('unknown comment...')
+    }
 
-        //
-        if(!(rows[0].user_id == req.session.user.user_id || config.adminUserId == req.session.user.user_id || rows[0].lead_mod == req.session.user.user_id)) {
-            return res.send('wrong user...')
-        }
+    //
+    if(!(rows[0].user_id == req.session.user.user_id || config.adminUserId == req.session.user.user_id || rows[0].lead_mod == req.session.user.user_id)) {
+        return res.send('wrong user...')
+    }
 
-        //
-        res.render(
-            'delete-comment',
-            {
-                html_title: htmlTitle,
-                user: req.session.user,
-                text_content: rows[0].text_content,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
-    })
-    .post(async (req, res) => {
+    //
+    res.render(
+        'delete-comment',
+        {
+            html_title: htmlTitle,
+            user: req.session.user,
+            text_content: rows[0].text_content,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
+}
+
+//
+const post = async (req, res) => {
             
-        //
-        if(!req.session.user) {
-            return res.send('nope...')
-        }
+    //
+    if(!req.session.user) {
+        return res.send('nope...')
+    }
 
-        //
-        const commentPublicId = req.params[0]
-        const {rows} = await db.getCommentWithPublic(commentPublicId)
+    //
+    const commentPublicId = req.params[0]
+    const {rows} = await db.getCommentWithPublic(commentPublicId)
 
-        //
-        if(!rows.length) {
-            return res.send('unknown comment...')
-        }
+    //
+    if(!rows.length) {
+        return res.send('unknown comment...')
+    }
 
-        //
-        if(!(rows[0].user_id == req.session.user.user_id || config.adminUserId == req.session.user.user_id || rows[0].lead_mod == req.session.user.user_id)) {
-            return res.send('wrong user...')
-        }
+    //
+    if(!(rows[0].user_id == req.session.user.user_id || config.adminUserId == req.session.user.user_id || rows[0].lead_mod == req.session.user.user_id)) {
+        return res.send('wrong user...')
+    }
 
-        await db.deleteComment(rows[0].path)
-        
-        return res.render(
-            'message',
-            {
-                html_title: htmlTitle,
-                message: "The comment and all of its sub comments (if any) were successfully deleted.",
-                user: req.session.user,
-                lead_mod_user_id: rows[0].lead_mod,
-                curr_castle: rows[0].castle,
-                max_width: myMisc.getCurrSiteMaxWidth(req)
-            })
-    })
+    await db.deleteComment(rows[0].path)
+    
+    return res.render(
+        'message',
+        {
+            html_title: htmlTitle,
+            message: "The comment and all of its sub comments (if any) were successfully deleted.",
+            user: req.session.user,
+            lead_mod_user_id: rows[0].lead_mod,
+            curr_castle: rows[0].castle,
+            max_width: myMisc.getCurrSiteMaxWidth(req)
+        })
+}
 
+//
+router.get('/', get)
+router.post('/', post)
 module.exports = router
