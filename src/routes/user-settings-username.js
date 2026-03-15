fa4c2ca60e16586f4fd53cf4d88b35aa46b80da1
Ry Ferguson <rytf12@gmail.com>
2026-03-15 22:16:28 +0000

    settings username: move router const to bottom of file

--- src/routes/user-settings-username.js
+++ src/routes/user-settings-username.js
@@ -4,7 +4,6 @@ const db = require('../db')
 const myMisc = require('../util/misc.js')
 const {isUser} = require('../middleware/is-user.js')
 
-const router = express.Router()
 const regexUsername = /^[a-z0-9-]{4,16}$/i
 const htmlTitle = 'Settings / Username'
 
@@ -75,6 +74,7 @@ const post = async (req, res) => {
 }
 
 //
+const router = express.Router()
 router.get('/', isUser, get)
 router.post('/', isUser, post)
 module.exports = router
