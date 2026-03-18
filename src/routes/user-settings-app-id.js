30be2461a28f1b4e229ffa65507decf668f2514a
Ry Ferguson <rytf12@gmail.com>
2026-03-18 22:41:16 +0000

    settings app id: use standard route function structure

--- src/routes/user-settings-app-id.js
+++ src/routes/user-settings-app-id.js
@@ -6,87 +6,92 @@ const myMisc = require('../util/misc.js')
 const router = express.Router()
 const htmlTitle = 'Settings / App ID'
 
-router.route('/')
-    .get(async (req, res) => {
+//
+const get = async (req, res) => {
 
-        //
-        if(!req.session.user) {
-            return res.redirect('/settings')
-        }
+    //
+    if(!req.session.user) {
+        return res.redirect('/settings')
+    }
 
-        //
-        const {rows} = await db.getClient(req.query.id)
+    //
+    const {rows} = await db.getClient(req.query.id)
 
-        //
-        if(rows.length == 0) {
-            return res.send('app does not exist')
-        }
+    //
+    if(rows.length == 0) {
+        return res.send('app does not exist')
+    }
 
-        //
-        const row = rows[0]
+    //
+    const row = rows[0]
 
-        //
-        if(row.user_id != req.session.user.user_id) {
-            return res.send('wrong user')
-        }
+    //
+    if(row.user_id != req.session.user.user_id) {
+        return res.send('wrong user')
+    }
 
-        //
-        renderHtml(req, res, row)
-    })
-    .post(async (req, res) => {
+    //
+    renderHtml(req, res, row)
+}
 
-        //
-        if(!req.session.user) {
-            return res.send('please log in')
-        }
+//
+const post = async (req, res) => {
 
-        //
-        const {rows} = await db.getClient(req.query.id)
+    //
+    if(!req.session.user) {
+        return res.send('please log in')
+    }
 
-        //
-        if(rows.length == 0) {
-            return res.send('app does not exist')
-        }
+    //
+    const {rows} = await db.getClient(req.query.id)
 
-        //
-        const row = rows[0]
+    //
+    if(rows.length == 0) {
+        return res.send('app does not exist')
+    }
 
-        //
-        if(row.user_id != req.session.user.user_id) {
-            return res.send('wrong user')
-        }
+    //
+    const row = rows[0]
 
-        //
-        const errors = myMisc.validateOauthClient(
+    //
+    if(row.user_id != req.session.user.user_id) {
+        return res.send('wrong user')
+    }
+
+    //
+    const errors = myMisc.validateOauthClient(
+        req.body.name,
+        req.body.ruri
+    )
+
+    const updatedRow = {
+        public_client_id: row.public_client_id,
+        app_name: req.body.name,
+        redirect_uri: req.body.ruri
+    }
+
+    if(errors.length > 0) {
+        renderHtml(req, res, updatedRow, errors)
+    }
+    else {
+        await db.updateClient(
+            row.client_id,
             req.body.name,
-            req.body.ruri
-        )
+            req.body.ruri)
+        
+        renderHtml(
+            req,
+            res,
+            updatedRow,
+            [],
+            'app info saved')
+    }
 
-        const updatedRow = {
-            public_client_id: row.public_client_id,
-            app_name: req.body.name,
-            redirect_uri: req.body.ruri
-        }
+}
 
-        if(errors.length > 0) {
-            renderHtml(req, res, updatedRow, errors)
-        }
-        else {
-            await db.updateClient(
-                row.client_id,
-                req.body.name,
-                req.body.ruri)
-            
-            renderHtml(
-                req,
-                res,
-                updatedRow,
-                [],
-                'app info saved')
-        }
-
-    })
-
+//
+router.get('/', get)
+router.post('/', post)
 module.exports = router
 
 //
