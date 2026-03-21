265a4a060ed1eea4c65b6dee8e309a5c6633bc37
Ry Ferguson <rytf12@gmail.com>
2026-03-21 23:01:09 +0000

    create apiPageValue() middleware

---
+++ src/middleware/api-page-value.js
@@ -0,0 +1,25 @@
+const myMisc = require('../util/misc.js')
+
+//
+function apiPageValue(req, res, next) {
+
+    //
+    let page
+
+    try {
+        page = myMisc.getPageNum(req)
+    }
+    catch(e) {
+        return res.status(400).json({
+            errors: [`Invalid pagination page value. ` +
+                `This value must be an integer between 1 and ${Number.MAX_SAFE_INTEGER}.`],
+        })
+    }
+
+    //
+    res.locals.page = page
+    next()
+}
+
+//
+module.exports.apiPageValue = apiPageValue
