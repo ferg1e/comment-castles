161def1a3473c579cf16aa4565b37205cd3d21c5
Ry Ferguson <rytf12@gmail.com>
2026-03-24 17:28:40 +0000

    api: put POST post endpoint in its own file

--- src/routes/api.js
+++ src/routes/api.js
@@ -172,51 +172,7 @@ router.get(
 )
 
 //
-router.post(
-    '/post',
-    async (req, res) => {
-
-        //
-        const title = (typeof req.body.title === 'undefined') ? '' : req.body.title
-        const text_content = (typeof req.body.text_content === 'undefined') ? '' : req.body.text_content
-        const link = (typeof req.body.link === 'undefined') ? '' : req.body.link
-        const sub = (typeof req.body.sub === 'undefined') ? '' : req.body.sub
-
-        //
-        const oauthData = await oauthAuthenticate(req, res)
-
-        //
-        if(!oauthData) {
-            return res.status(401).json({
-                errors: ['invalid or no user auth'],
-            })
-        }
-
-        //
-        const [errors, wsCompressedTitle, trimSub] = await db.validateNewPost(
-            title,
-            link,
-            sub)
-
-        //
-        if(errors.length) {
-            return res.status(400).json({
-                errors: errors,
-            })
-        }
-
-        //
-        const newPost = await db.createPost(
-            oauthData.user.user_id,
-            wsCompressedTitle,
-            text_content,
-            link,
-            trimSub)
-
-        //
-        return res.json(newPost)
-    }
-)
+router.post('/post', require('./api-post-post'))
 
 // edit post
 router.put(
