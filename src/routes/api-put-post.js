93524256b704f22f29713b2f9c930e098ed653e1
Ry Ferguson <rytf12@gmail.com>
2026-03-28 16:22:22 +0000

    api: move route function for api PUT /post into a separate file

---
+++ src/routes/api-put-post.js
@@ -0,0 +1,83 @@
+const db = require('../db')
+const {oauthAuthenticate} = require('../util/oauth-authenticate')
+
+//
+const put = async (req, res) => {
+
+    //
+    const oauthData = await oauthAuthenticate(req, res)
+
+    //
+    if(!oauthData) {
+        return res.status(401).json({
+            errors: ['invalid or no user auth'],
+        })
+    }
+
+    //
+    if(typeof req.body.post_id === 'undefined') {
+        return res.status(400).json({
+            errors: ['no post_id in body'],
+        })
+    }
+
+    //
+    const postPublicId = req.body.post_id
+    const {rows:[row]} = await db.getPostWithPublic(postPublicId)
+
+    //
+    if(!row) {
+        return res.status(404).json({
+            errors: ["no post with that postid"],
+        })
+    }
+
+    //
+    if(row.user_id != oauthData.user.user_id) {
+        return res.status(403).json({
+            errors: ["wrong user"],
+        })
+    }
+
+    //
+    const fTitle = typeof req.body.title === 'undefined'
+        ? row.title
+        : req.body.title
+
+    //
+    const fLink = typeof req.body.link === 'undefined'
+        ? (row.link === null ? '' : row.link)
+        : req.body.link
+
+    //
+    const fTextContent = typeof req.body.text_content === 'undefined'
+        ? (row.text_content === null ? '' : row.text_content)
+        : req.body.text_content
+
+    //
+    const [errors, wsCompressedTitle] = await db.validateEditPost(
+        fTitle,
+        fLink)
+
+    //
+    if(errors.length) {
+        return res.status(400).json({
+            errors: errors,
+        })
+    }
+
+    //
+    await db.updatePost(
+        row.post_id,
+        wsCompressedTitle,
+        fTextContent,
+        fLink)
+
+    //
+    return res.json({
+        post_id: postPublicId,
+    })
+}
+
+//
+module.exports = put
