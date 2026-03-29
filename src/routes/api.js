c8b293910405e5919ac1c86ba665ba4b85b4b4a3
Ry Ferguson <rytf12@gmail.com>
2026-03-29 23:10:12 +0100

    api: remove all code that's no longer used in api.js due to all endpoint route functions being moved to separate files

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,18 +1,7 @@
 const express = require('express')
-const db = require('../db')
-const myMisc = require('../util/misc.js')
-const config = require('../config')
-const OAuth2Server = require('oauth2-server')
-const Request = require('oauth2-server').Request
-const Response = require('oauth2-server').Response
 
 //
 const router = express.Router()
-
-//
-const oauth = new OAuth2Server({
-    model: require('../oauth-model.js')
-})
 
 //
 router.get('/posts', require('./api-get-posts'))
@@ -46,23 +35,3 @@ router.delete('/comment', require('./api-delete-comment'))
 
 //
 module.exports = router
-
-//
-async function oauthAuthenticate(req, res) {
-    const request = new Request(req)
-    const response = new Response(res)
-    const options = {}
-    let oauthData = null
-
-    try {
-        oauthData = await oauth.authenticate(request, response, options)
-    }
-    catch(e) {
-        // basically no access token in header
-        // or wrong access token in header
-        // either way, do nothing and proceed
-        // with API call render
-    }
-
-    return oauthData
-}
