004206d6421d3bb315f8ceea9a59fd0b503e4961
Ry Ferguson <rytf12@gmail.com>
2026-03-29 23:18:44 +0100

    api: organize endpoints in api.js by adding some comments and deleting some newlines

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,34 +3,22 @@ const express = require('express')
 //
 const router = express.Router()
 
-//
+// get all posts
 router.get('/posts', require('./api-get-posts'))
 
-//
+// get all posts in a sub
 router.get('/sub/posts', require('./api-get-sub-posts'))
 
-//
+// get, create, edit and delete a single post
 router.get('/post', require('./api-get-post'))
-
-//
 router.post('/post', require('./api-post-post'))
-
-// edit post
 router.put('/post', require('./api-put-post'))
-
-// delete post
 router.delete('/post', require('./api-delete-post'))
 
-//
+// get, create, edit and delete a single comment
 router.get('/comment', require('./api-get-comment'))
-
-//
 router.post('/comment', require('./api-post-comment'))
-
-// edit comment
 router.put('/comment', require('./api-put-comment'))
-
-// delete comment
 router.delete('/comment', require('./api-delete-comment'))
 
 //
