697b37ffe302c42d9fecb1f4afdcce7ef607e3b9
Ry Ferguson <rytf12@gmail.com>
2026-04-07 22:43:40 +0100

    oauth: move route for /oauth/token into a separate file

--- app.js
+++ app.js
@@ -88,5 +88,5 @@ app.use(/^\/dm\/([a-z0-9]{22})\/delete$/i, require('./src/routes/single-dm-delet
 app.use('/api/v1/', require('./src/routes/api'))
 app.use('/api/ajax/', require('./src/routes/ajax'))
 app.use('/oauth/authorize/', require('./src/routes/oauth-authorize'))
-app.use('/oauth/', require('./src/routes/oauth'))
+app.use('/oauth/token/', require('./src/routes/oauth-token'))
 app.use('*', require('./src/routes/fallthrough'))
---
+++ src/routes/oauth-token.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const OAuth2Server = require('oauth2-server')
+const Request = require('oauth2-server').Request
+const Response = require('oauth2-server').Response
+const db = require('../db')
+const crypto = require('crypto')
+
+const router = express.Router()
+
+//
+const oauth = new OAuth2Server({
+    model: require('../oauth-model.js')
+})
+
+//
+router.route('/')
+    .post(async (req, res) => {
+
+        /*
+        --start PKCE check--
+        only emit PKCE errors here
+        because oauth.token() after will
+        handle the rest.
+        */
+        const clientId = req.body.client_id
+        const redirectUri = req.body.redirect_uri
+        const authCode = req.body.code
+        const isReadyForPkceCheck =
+            typeof clientId != 'undefined' &&
+            typeof redirectUri != 'undefined' &&
+            typeof authCode != 'undefined'
+
+        if(isReadyForPkceCheck) {
+            const {rows:[dbAuthCode]} = await db.getAuthCode(authCode)
+
+            //
+            if(dbAuthCode) {
+                const isRecordMatch = dbAuthCode.public_client_id == clientId &&
+                    dbAuthCode.redirect_uri == redirectUri
+                
+                //
+                if(isRecordMatch) {
+                    const codeVerifier = req.body.code_verifier
+
+                    //
+                    if(typeof codeVerifier == 'undefined') {
+                        return res.status(400).json({
+                            errors: ['no code_verifier in body'],
+                        })
+                    }
+
+                    //
+                    let isGoodPkce = false
+
+                    if(dbAuthCode.cc_method == 'plain') {
+                        isGoodPkce = dbAuthCode.code_challenge == codeVerifier
+                    }
+                    else {
+                        const nowHash = crypto
+                            .createHash('sha256')
+                            .update(codeVerifier)
+                            .digest('base64')
+                            .replace(/=/g, '')
+                            .replace(/\+/g, '-')
+                            .replace(/\//g, '_')
+
+                        isGoodPkce = dbAuthCode.code_challenge == nowHash
+                    }
+
+                    if(!isGoodPkce) {
+                        return res.status(400).json({
+                            errors: ['invalid PKCE code verifier'],
+                        })
+                    }
+                }
+            }
+        }
+        //--end PKCE check--
+
+        //
+        const request = new Request(req);
+        const response = new Response(res);
+        const options = {
+            requireClientAuthentication: {
+                authorization_code: false
+            }
+        }
+
+        //
+        oauth.token(request, response, options)
+            .then((token) => {
+                res.set(response.headers)
+                res.status(response.status)
+                return res.json(response.body)
+            })
+            .catch((error) => {
+                return res.status(400).json({
+                    errors: [`the following error ocurred: ${error.message}`],
+                })
+            })
+    })
+
+module.exports = router
