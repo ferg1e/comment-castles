cf9f5235a0960ef5aa36dafa0b959d2a65e6e890
Ry Ferguson <rytf12@gmail.com>
2026-04-15 23:54:19 +0100

    delete dm: use isDmOwner middleware on GET and POST routes

--- src/routes/single-dm-delete.js
+++ src/routes/single-dm-delete.js
@@ -3,6 +3,7 @@ const db = require('../db')
 const myMisc = require('../util/misc.js')
 const {isUser} = require('../middleware/is-user')
 const {checkDm} = require('../middleware/check-dm')
+const {isDmOwner} = require('../middleware/is-dm-owner')
 
 //
 const htmlTitle = 'Delete DM'
@@ -12,11 +13,6 @@ const get = async (req, res) => {
 
     //
     const dm = res.locals.dm
-
-    //
-    if(dm.from_user_id != req.session.user.user_id) {
-        return res.send('wrong user...')
-    }
 
     //
     return res.render('delete-dm', {
@@ -34,10 +30,6 @@ const post = async (req, res) => {
     const dm = res.locals.dm
 
     //
-    if(dm.from_user_id != req.session.user.user_id) {
-        return res.send('wrong user...')
-    }
-
     await db.deleteDm(dm.dm_id)
     
     return res.render('message', {
@@ -50,6 +42,6 @@ const post = async (req, res) => {
 
 //
 const router = express.Router({mergeParams: true})
-router.get('/', isUser, checkDm, get)
-router.post('/', isUser, checkDm, post)
+router.get('/', isUser, checkDm, isDmOwner, get)
+router.post('/', isUser, checkDm, isDmOwner, post)
 module.exports = router
